509430b68326d6d462af04c15fce369e3ed2ad0f
Alistair Johnson <alistairewj@gmail.com>
2017-03-17 19:30:04 -0400

    data calc based of apache rules

--- mimic-iii/sql/bg-first-day.sql
+++ mimic-iii/sql/bg-first-day.sql
@@ -4,169 +4,161 @@
 -- now generate arterial only blood gas samples
 DROP TABLE IF EXISTS gosiss_bg_d1 CASCADE;
 CREATE TABLE gosiss_bg_d1 AS
-with stg_spo2 as
+with oxy_stg as
 (
-  select SUBJECT_ID, HADM_ID, ICUSTAY_ID, CHARTTIME
-    -- max here is just used to group SpO2 by charttime
-    , max(case when valuenum <= 0 or valuenum > 100 then null else valuenum end) as SpO2
-  from CHARTEVENTS
-  -- o2 sat
-  where ITEMID in
-  (
-    646 -- SpO2
-  , 220277 -- O2 saturation pulseoxymetry
-  )
-  group by SUBJECT_ID, HADM_ID, ICUSTAY_ID, CHARTTIME
-)
-, stg_fio2 as
-(
-  select SUBJECT_ID, HADM_ID, ICUSTAY_ID, CHARTTIME
-    -- pre-process the FiO2s to ensure they are between 21-100%
-    , max(
+  select bg.icustay_id, bg.charttime
+  -- calculate score for all blood gases
+  , po2 as pao2
+  , pco2 as paco2
+  , fio2
+  , aado2
+  , case
+      when PO2 is null and AaDO2 is null
+        then null
+      when fio2 >= 0.5 and AaDO2 is not null then
         case
-          when itemid = 223835
-            then case
-              when valuenum > 0.2 and valuenum <= 1
-                then valuenum * 100
-              -- improperly input data - looks like O2 flow in litres
-              when valuenum > 1 and valuenum < 21
-                then null
-              when valuenum >= 21 and valuenum <= 100
-                then valuenum
-              else null end -- unphysiological
-        when itemid in (3420, 3422)
-        -- all these values are well formatted
-            then valuenum
-        when itemid = 190 and valuenum > 0.20 and valuenum < 1
-        -- well formatted but not in %
-            then valuenum * 100
-      else null end
-    ) as fio2_chartevents
-  from CHARTEVENTS
-  where ITEMID in
-  (
-    3420 -- FiO2
-  , 190 -- FiO2 set
-  , 223835 -- Inspired O2 Fraction (FiO2)
-  , 3422 -- FiO2 [measured]
-  )
-  -- exclude rows marked as error
-  and error IS DISTINCT FROM 1
-  group by SUBJECT_ID, HADM_ID, ICUSTAY_ID, CHARTTIME
+          when AaDO2 <  100 then 0
+          when AaDO2 <  250 then 7
+          when AaDO2 <  350 then 9
+          when AaDO2 <  500 then 11
+          when AaDO2 >= 500 then 14
+        else 0 end
+      -- here we make an implicit assumption that no fio2 obs means fio2 < 0.5
+      when PO2 is not null then
+        case
+          when PO2 < 50 then 15
+          when PO2 < 70 then 5
+          when PO2 < 80 then 2
+        else 0 end
+      else null
+    end as oxygenation_score
+  from gosiss_bg bg
+  where specimen_pred = 'ART'
+  and (po2 is not null or aado2 is not null)
 )
-, stg2 as
+, oxy_score as
 (
-select bg.*
-  , ROW_NUMBER() OVER (partition by bg.icustay_id, bg.charttime order by s1.charttime DESC) as lastRowSpO2
-  , s1.spo2
-from gosiss_bg bg
-left join stg_spo2 s1
-  -- same patient
-  on  bg.icustay_id = s1.icustay_id
-  -- spo2 occurred at most 2 hours before this blood gas
-  and s1.charttime between bg.charttime - interval '2' hour and bg.charttime
-where bg.po2 is not null
+  select
+    icustay_id, charttime
+    , pao2, paco2, fio2, aado2
+    , oxygenation_score
+    , ROW_NUMBER() OVER (PARTITION BY icustay_id ORDER BY oxygenation_score desc)
+        as rn
+  from oxy_stg
 )
-, stg3 as
+-- because ph/pco2 rules are an interaction *within* a blood gas, we calculate them here
+-- the worse score is then taken for the final calculation
+, acidbase as
 (
-select bg.*
-  , ROW_NUMBER() OVER (partition by bg.icustay_id, bg.charttime order by s2.charttime DESC) as lastRowFiO2
-  , s2.fio2_chartevents
-
-  -- create our specimen prediction
-  ,  1/(1+exp(-(-0.02544
-  +    0.04598 * po2
-  + coalesce(-0.15356 * spo2             , -0.15356 *   97.49420 +    0.13429)
-  + coalesce( 0.00621 * fio2_chartevents ,  0.00621 *   51.49550 +   -0.24958)
-  + coalesce( 0.10559 * hemoglobin       ,  0.10559 *   10.32307 +    0.05954)
-  + coalesce( 0.13251 * so2              ,  0.13251 *   93.66539 +   -0.23172)
-  + coalesce(-0.01511 * pco2             , -0.01511 *   42.08866 +   -0.01630)
-  + coalesce( 0.01480 * fio2             ,  0.01480 *   63.97836 +   -0.31142)
-  + coalesce(-0.00200 * aado2            , -0.00200 *  442.21186 +   -0.01328)
-  + coalesce(-0.03220 * bicarbonate      , -0.03220 *   22.96894 +   -0.06535)
-  + coalesce( 0.05384 * totalco2         ,  0.05384 *   24.72632 +   -0.01405)
-  + coalesce( 0.08202 * lactate          ,  0.08202 *    3.06436 +    0.06038)
-  + coalesce( 0.10956 * ph               ,  0.10956 *    7.36233 +   -0.00617)
-  + coalesce( 0.00848 * o2flow           ,  0.00848 *    7.59362 +   -0.35803)
-  ))) as SPECIMEN_PROB
-from stg2 bg
-left join stg_fio2 s2
-  -- same patient
-  on  bg.icustay_id = s2.icustay_id
-  -- fio2 occurred at most 4 hours before this blood gas
-  and s2.charttime between bg.charttime - interval '4' hour and bg.charttime
-where bg.lastRowSpO2 = 1 -- only the row with the most recent SpO2 (if no SpO2 found lastRowSpO2 = 1)
+  select bg.icustay_id
+  , ph
+  , pco2 as paco2
+  , case
+      when ph is null or pco2 is null then null
+      when ph < 7.20 then
+        case
+          when pco2 < 50 then 12
+          else 4
+        end
+      when ph < 7.30 then
+        case
+          when pco2 < 30 then 9
+          when pco2 < 40 then 6
+          when pco2 < 50 then 3
+          else 2
+        end
+      when ph < 7.35 then
+        case
+          when pco2 < 30 then 9
+          when pco2 < 45 then 0
+          else 1
+        end
+      when ph < 7.45 then
+        case
+          when pco2 < 30 then 5
+          when pco2 < 45 then 0
+          else 1
+        end
+      when ph < 7.50 then
+        case
+          when pco2 < 30 then 5
+          when pco2 < 35 then 0
+          when pco2 < 45 then 2
+          else 12
+        end
+      when ph < 7.60 then
+        case
+          when pco2 < 40 then 3
+          else 12
+        end
+      else -- ph >= 7.60
+        case
+          when pco2 < 25 then 0
+          when pco2 < 40 then 3
+          else 12
+        end
+    end as acidbase_score
+  from gosiss_bg bg
+  where ph is not null and pco2 is not null
+  and specimen_pred = 'ART'
 )
-select subject_id, hadm_id, icustay_id
--- , charttime
--- , SPECIMEN -- raw data indicating sample type, only present 80% of the time
+, ab_score as
+(
+  select icustay_id, acidbase_score, ph, paco2
+    -- create integer which indexes maximum value of score with 1
+  , ROW_NUMBER() over (partition by ICUSTAY_ID ORDER BY ACIDBASE_SCORE DESC)
+      as rn
+  from acidbase
+)
+select ie.subject_id, ie.hadm_id, ie.icustay_id
+-- aggregation functions here are just cosmetic
+-- the join has "rn=1" which ensures only 1 row exists
+, min(oxy_score.pao2) as aps3_oxy_pao2
+, min(oxy_score.paco2) as aps3_oxy_paco2
+, min(oxy_score.fio2) as aps3_oxy_fio2
+, min(oxy_score.aado2) as aps3_oxy_aado2
+, min(oxy_score.oxygenation_score) as oxygenation_score
 
--- prediction of specimen for missing data
--- , case
---       when SPECIMEN is not null then SPECIMEN
---       when SPECIMEN_PROB > 0.75 then 'ART'
---     else null end as SPECIMEN_PRED
--- , SPECIMEN_PROB
+, min(ab_score.ph) as aps3_acidbase_ph
+, min(ab_score.paco2) as aps3_acidbase_paco2
+, min(ab_score.acidbase_score) as acidbase_score
 
--- oxygen related parameters
--- , SO2, spo2 -- note spo2 is from chartevents
-, max(PO2) as PO2_max
-, max(PCO2) as PCO2_max
-, min(PO2) as PO2_min
-, min(PCO2) as PCO2_min
--- , fio2_chartevents, FIO2
--- , AADO2
--- also calculate AADO2
--- , case
---     when  PO2 is not null
---       and pco2 is not null
---       and coalesce(FIO2, fio2_chartevents) is not null
---      -- multiple by 100 because FiO2 is in a % but should be a fraction
---       then (coalesce(FIO2, fio2_chartevents)/100) * (760 - 47) - (pco2/0.8) - po2
---     else null
---   end as AADO2_calc
-, min(case
-    when PO2 is not null and coalesce(FIO2, fio2_chartevents) is not null
-     -- multiply by 100 because FiO2 is in a % but should be a fraction
-      then 100*PO2/(coalesce(FIO2, fio2_chartevents))
-    else null
-  end) as PaO2FiO2_min
 -- acid-base parameters
-, max(PH) as PH_max
-, min(PH) as PH_min
+, min(bg.PH) as PH_min
+, max(bg.PH) as PH_max
 
-, min(BASEEXCESS) as BASEEXCESS_min
-, max(BASEEXCESS) as BASEEXCESS_max
-, min(BICARBONATE) as BICARBONATE_min
-, max(BICARBONATE) as BICARBONATE_max
-, min(TOTALCO2) as TOTALCO2_min
-, max(TOTALCO2) as TOTALCO2_max
+, min(bg.BASEEXCESS) as BASEEXCESS_min
+, max(bg.BASEEXCESS) as BASEEXCESS_max
+, min(bg.BICARBONATE) as BICARBONATE_min
+, max(bg.BICARBONATE) as BICARBONATE_max
+, min(bg.TOTALCO2) as TOTALCO2_min
+, max(bg.TOTALCO2) as TOTALCO2_max
 
 -- blood count parameters
-, min(HEMATOCRIT) as HEMATOCRIT_min
-, max(HEMATOCRIT) as HEMATOCRIT_max
-, min(HEMOGLOBIN) as HEMOGLOBIN_min
-, max(HEMOGLOBIN) as HEMOGLOBIN_max
-, min(CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_min
-, max(CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_max
-, min(METHEMOGLOBIN) as METHEMOGLOBIN_min
-, max(METHEMOGLOBIN) as METHEMOGLOBIN_max
+, min(bg.HEMATOCRIT) as HEMATOCRIT_min
+, max(bg.HEMATOCRIT) as HEMATOCRIT_max
+, min(bg.HEMOGLOBIN) as HEMOGLOBIN_min
+, max(bg.HEMOGLOBIN) as HEMOGLOBIN_max
+, min(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_min
+, max(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_max
+, min(bg.METHEMOGLOBIN) as METHEMOGLOBIN_min
+, max(bg.METHEMOGLOBIN) as METHEMOGLOBIN_max
 
 -- chemistry
-, min(CHLORIDE) as CHLORIDE_min
-, max(CHLORIDE) as CHLORIDE_max
-, min(CALCIUM) as CALCIUM_min
-, max(CALCIUM) as CALCIUM_max
-, min(TEMPERATURE) as TEMPERATURE_min
-, max(TEMPERATURE) as TEMPERATURE_max
-, min(POTASSIUM) as POTASSIUM_min
-, max(POTASSIUM) as POTASSIUM_max
-, min(SODIUM) as SODIUM_min
-, max(SODIUM) as SODIUM_max
-, min(LACTATE) as LACTATE_min
-, max(LACTATE) as LACTATE_max
-, min(GLUCOSE) as GLUCOSE_min
-, max(GLUCOSE) as GLUCOSE_max
+, min(bg.CHLORIDE) as CHLORIDE_min
+, max(bg.CHLORIDE) as CHLORIDE_max
+, min(bg.CALCIUM) as CALCIUM_min
+, max(bg.CALCIUM) as CALCIUM_max
+, min(bg.TEMPERATURE) as TEMPERATURE_min
+, max(bg.TEMPERATURE) as TEMPERATURE_max
+, min(bg.POTASSIUM) as POTASSIUM_min
+, max(bg.POTASSIUM) as POTASSIUM_max
+, min(bg.SODIUM) as SODIUM_min
+, max(bg.SODIUM) as SODIUM_max
+, min(bg.LACTATE) as LACTATE_min
+, max(bg.LACTATE) as LACTATE_max
+, min(bg.GLUCOSE) as GLUCOSE_min
+, max(bg.GLUCOSE) as GLUCOSE_max
 
 -- ventilation stuff that's sometimes input
 -- , INTUBATED
@@ -177,9 +169,16 @@ select subject_id, hadm_id, icustay_id
 -- , O2Flow
 -- , REQUIREDO2
 
-from stg3
-where lastRowFiO2 = 1 -- only the most recent FiO2
--- restrict it to *only* arterial samples
---and (SPECIMEN = 'ART' or SPECIMEN_PROB > 0.75)
-group by subject_id, hadm_id, icustay_id --, charttime
-order by icustay_id;
+from icustays ie
+left join gosiss_bg bg
+  on ie.icustay_id = bg.icustay_id
+  -- restrict it to *only* arterial samples
+  and bg.specimen_pred = 'ART'
+left join ab_score
+  on ie.icustay_id = ab_score.icustay_id
+  and ab_score.rn = 1
+left join oxy_score
+  on ie.icustay_id = oxy_score.icustay_id
+  and oxy_score.rn = 1
+group by ie.subject_id, ie.hadm_id, ie.icustay_id
+order by ie.icustay_id;
