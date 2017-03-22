3f435eb18b93a6657b565e04cc1c15cef65787e6
Alistair Johnson <alistairewj@gmail.com>
2017-03-22 15:31:55 -0400

    refactor and add service

--- mimic-iii/sql/demographics.sql
+++ mimic-iii/sql/demographics.sql
@@ -2,69 +2,107 @@
 
 DROP TABLE IF EXISTS gosiss_demographics CASCADE;
 CREATE TABLE gosiss_demographics AS
-with demo as
+with demo_stg as
 (
   SELECT ie.subject_id, ie.hadm_id, ie.icustay_id
   -- here we assign labels to ITEMIDs
   -- this also fuses together multiple ITEMIDs containing the same data
   , CASE
-        WHEN itemid = 3580 THEN 'WEIGHT' --very low decimal
-	WHEN itemid = 763 THEN 'WEIGHT'  --reasonable value
-	WHEN itemid = 3693 THEN 'WEIGHT' --low decimal
-	WHEN itemid = 224639 THEN ' WEIGHT' --reasonable value
-        WHEN itemid = 1394 THEN 'HEIGHT'
-	WHEN itemid = 226707 THEN 'HEIGHT'
-        WHEN itemid = 225082 THEN 'PREGNANT'
-        WHEN itemid = 227687 THEN 'SMOKING'
-        WHEN itemid = 225108 THEN 'SMOKING'
-      ELSE null
+    	WHEN itemid in (762, 763, 226512, 224639) THEN 'WEIGHT'
+      WHEN itemid in (920, 1394, 226707) THEN 'HEIGHT'
+      WHEN itemid = 225082 THEN 'PREGNANT'
+      WHEN itemid = 227687 THEN 'SMOKING'
+      WHEN itemid = 225108 THEN 'SMOKING'
+    ELSE null
     END AS label
   , -- the values with sanity checks for non-flags to not be 0 nor negative, and flags to not be negative
     CASE
-      WHEN itemid = 3580 and valuenum <=    0 THEN null -- kg 'WEIGHT'
-      WHEN itemid = 763 and valuenum <= 0 THEN null -- kg 'WEIGHT'
-      WHEN itemid = 3693 and valuenum <= 0 THEN null -- kg 'WEIGHT'
-      WHEN itemid = 224639 and valuenum <= 0 THEN null -- kg 'WEIGHT'
-      WHEN itemid = 1394 and valuenum <= 40 THEN null -- in 'HEIGHT'
-      WHEN itemid = 226707 and valuenum <= 40 THEN null -- in 'HEIGHT'
+      -- rules for weight
+      WHEN itemid in (762, 763, 226512, 224639)
+        and (valuenum > 400 or valuenum < 20) THEN null
+      -- rules for height
+      WHEN itemid in (920, 1394, 226707)
+        and (valuenum > 100 or valuenum < 10) THEN null
       WHEN itemid = 225082 and valuenum < 0 THEN null -- flag 'PREGNANT'
-      WHEN itemid = 227687 and valuenum < 0 THEN null -- flag 'SMOKING'
-      WHEN itemid = 225108 and valuenum < 0 THEN null -- flag 'SMOKING'
-      WHEN itemid = 225108 and valuenum = 1 THEN 2    -- flag 'SMOKING' if use = 1 then 2,
-						      -- if history = 1 and use = 0 then 1, if either = 0 then 0
+      -- smoking usage is coded as 0/1/2
+       -- if never, 0
+       -- if previous user, then 1
+       -- if current user, then 2
+      WHEN itemid = 227687 and valuenum < 0 THEN null
+      WHEN itemid = 225108 and valuenum < 0 THEN null
+      WHEN itemid = 225108 and valuenum = 1 THEN 2
     ELSE ce.valuenum
     END AS valuenum
-
   FROM icustays ie
-
   LEFT JOIN chartevents ce
-    ON ce.subject_id = ie.subject_id AND ce.hadm_id = ie.hadm_id
+    ON ie.icustay_id = ce.icustay_id
     AND ce.ITEMID in
     (
-      -- comment is: LABEL | UNITS | DBSOURCE
-      3580, -- PRESENT WEIGHT (KG) | KG | CAREVUE
+      -- CAREVUE - weight
+      762, -- ADMISSION WEIGHT (KG) | CAREVUE
       763, -- DAILY WEIGHT | KG | CAREVUE
-      3693, -- WEIGHT KG | KG | CAREVUE
+
+      -- many ITEMIDs are used for neonates, so not included here
+      -- 3723 - birth weight
+      -- 3580, -- PRESENT WEIGHT (KG) - used for neonates
+      -- 3581 and 3582 are copies of 3580 for different units.
+      -- 3693, -- Weight KG - but used for neonates
+
+      -- METAVISION - weight
+      226512, -- Admission Weight (Kg)
       224639, -- DAILY WEIGHT | KG | METAVISION
-      1394, -- HEIGHT INCHES | IN | CAREVUE
+
+      -- CAREVUE - height
+      920, -- Admission height (inches)
+      1394, -- HEIGHT INCHES
+
+      -- below used for neonates
+      -- 3485, -- length calc (cm)
+      -- 3486, -- length in inches
+      -- 4187, -- length calc inches (but UOM in chartevents is cm) - data looks like inches
+      -- 4188, -- length in cm (but UOM in chartevents is inches) - data looks like cm
+
       226707, -- HEIGHT | IN | METAVISION
       225082, -- PREGNANT | FLAG | METAVISION
       227687, -- TOBACCO USE HISTORY | FLAG | METAVISION
       225108 -- TOBACCO USE | FLAG | METAVISION
     )
     AND valuenum IS NOT null
+    AND COALESCE(error,0)!=1
+    AND ce.charttime < ie.intime + interval '1' day
 )
-SELECT
-  demo.subject_id, demo.hadm_id, demo.icustay_id
-
-  , max(CASE WHEN label = 'WEIGHT' THEN valuenum ELSE null END) as WEIGHT
-  , max(CASE WHEN label = 'HEIGHT' THEN valuenum ELSE null END) as HEIGHT
+, demo as
+(
+  select icustay_id
+  , avg(CASE WHEN label = 'WEIGHT' THEN valuenum ELSE null END) as WEIGHT
+  , avg(CASE WHEN label = 'HEIGHT' THEN valuenum ELSE null END) as HEIGHT
   , max(CASE WHEN label = 'PREGNANT' THEN valuenum ELSE null END) as PREGNANT
   , max(CASE WHEN label = 'SMOKING' THEN valuenum ELSE null END) as SMOKING
-
-FROM demo
+  from demo_stg
+  group by icustay_id
+)
+, serv as
+(
+  select ie.icustay_id, se.curr_service as first_service
+  , ROW_NUMBER() over (PARTITION BY ie.hadm_id ORDER BY se.transfertime DESC) as rn
+  from icustays ie
+  left join services se
+    on ie.hadm_id = se.hadm_id
+    and se.transfertime < ie.intime + interval '1' day
+)
+SELECT
+  ie.subject_id, ie.hadm_id, ie.icustay_id
+  , demo.WEIGHT
+  , demo.HEIGHT
+  , demo.PREGNANT
+  , demo.SMOKING
+  , serv.first_service
+FROM icustays ie
+LEFT JOIN demo
+  on ie.icustay_id = demo.icustay_id
+LEFT JOIN serv
+  on demo.icustay_id = serv.icustay_id
+  and serv.rn = 1
 
 GROUP BY demo.subject_id, demo.hadm_id, demo.icustay_id
 ORDER BY demo.subject_id, demo.hadm_id, demo.icustay_id;
-
-commit;
