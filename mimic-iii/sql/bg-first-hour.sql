6eab5af72076e042d80dcbd32c8de20b71b68df4
Alistair Johnson <alistairewj@gmail.com>
2017-04-12 10:08:41 -0400

    refactor - remove unecessary group fcns for clarity

--- mimic-iii/sql/bg-first-hour.sql
+++ mimic-iii/sql/bg-first-hour.sql
@@ -122,65 +122,149 @@ with bg as
       as rn
   from acidbase
 )
-select ie.subject_id, ie.hadm_id, ie.icustay_id
+, bg_agg as
+(
+  select ie.subject_id, ie.hadm_id, ie.icustay_id
+  -- aggregation functions here are just cosmetic
+  -- the join has "rn=1" which ensures only 1 row exists
+  , min(oxy_score.pao2) as aps3_oxy_pao2
+  , min(oxy_score.paco2) as aps3_oxy_paco2
+  , min(oxy_score.fio2) as aps3_oxy_fio2
+  , min(oxy_score.aado2) as aps3_oxy_aado2
+  , min(oxy_score.oxygenation_score) as oxygenation_score
+
+  , min(ab_score.ph) as aps3_acidbase_ph
+  , min(ab_score.paco2) as aps3_acidbase_paco2
+  , min(ab_score.acidbase_score) as acidbase_score
+
+  -- oxygenation parameters
+  , min(bg.po2) as pao2_min
+  , max(bg.po2) as pao2_max
+  , min(bg.pco2) as paco2_min
+  , max(bg.pco2) as paco2_max
+  , min(bg.pao2fio2) as pao2fio2_min
+  , max(bg.pao2fio2) as pao2fio2_max
+  , min(coalesce(bg.aado2, bg.aado2_calc)) as aado2_min
+  , max(coalesce(bg.aado2, bg.aado2_calc)) as aado2_max
+
+  -- acid-base parameters
+  , min(bg.PH) as PH_min
+  , max(bg.PH) as PH_max
+
+  , min(bg.BASEEXCESS) as BASEEXCESS_min
+  , max(bg.BASEEXCESS) as BASEEXCESS_max
+  , min(bg.BICARBONATE) as BICARBONATE_min
+  , max(bg.BICARBONATE) as BICARBONATE_max
+  , min(bg.TOTALCO2) as TOTALCO2_min
+  , max(bg.TOTALCO2) as TOTALCO2_max
+
+  -- blood count parameters
+  , min(bg.HEMATOCRIT) as HEMATOCRIT_min
+  , max(bg.HEMATOCRIT) as HEMATOCRIT_max
+  , min(bg.HEMOGLOBIN) as HEMOGLOBIN_min
+  , max(bg.HEMOGLOBIN) as HEMOGLOBIN_max
+  , min(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_min
+  , max(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_max
+  , min(bg.METHEMOGLOBIN) as METHEMOGLOBIN_min
+  , max(bg.METHEMOGLOBIN) as METHEMOGLOBIN_max
+
+  -- chemistry
+  , min(bg.CHLORIDE) as CHLORIDE_min
+  , max(bg.CHLORIDE) as CHLORIDE_max
+  , min(bg.CALCIUM) as CALCIUM_min
+  , max(bg.CALCIUM) as CALCIUM_max
+  , min(bg.TEMPERATURE) as TEMPERATURE_min
+  , max(bg.TEMPERATURE) as TEMPERATURE_max
+  , min(bg.POTASSIUM) as POTASSIUM_min
+  , max(bg.POTASSIUM) as POTASSIUM_max
+  , min(bg.SODIUM) as SODIUM_min
+  , max(bg.SODIUM) as SODIUM_max
+  , min(bg.LACTATE) as LACTATE_min
+  , max(bg.LACTATE) as LACTATE_max
+  , min(bg.GLUCOSE) as GLUCOSE_min
+  , max(bg.GLUCOSE) as GLUCOSE_max
+
+  -- ventilation stuff that's sometimes input
+  -- , INTUBATED
+  -- , TIDALVOLUME
+  -- , VENTILATIONRATE
+  -- , VENTILATOR
+  -- , PEEP
+  -- , O2Flow
+  -- , REQUIREDO2
+
+  from icustays ie
+  left join bg
+    on ie.icustay_id = bg.icustay_id
+    -- restrict it to *only* arterial samples
+    and bg.specimen_pred = 'ART'
+  left join ab_score
+    on ie.icustay_id = ab_score.icustay_id
+    and ab_score.rn = 1
+  left join oxy_score
+    on ie.icustay_id = oxy_score.icustay_id
+    and oxy_score.rn = 1
+  group by ie.subject_id, ie.hadm_id, ie.icustay_id
+)
+select bg_agg.subject_id, bg_agg.hadm_id, bg_agg.icustay_id
 -- aggregation functions here are just cosmetic
 -- the join has "rn=1" which ensures only 1 row exists
-, min(oxy_score.pao2) as aps3_oxy_pao2
-, min(oxy_score.paco2) as aps3_oxy_paco2
-, min(oxy_score.fio2) as aps3_oxy_fio2
-, min(oxy_score.aado2) as aps3_oxy_aado2
-, min(oxy_score.oxygenation_score) as oxygenation_score
+, (oxy_score.pao2) as aps3_oxy_pao2
+, (oxy_score.paco2) as aps3_oxy_paco2
+, (oxy_score.fio2) as aps3_oxy_fio2
+, (oxy_score.aado2) as aps3_oxy_aado2
+, (oxy_score.oxygenation_score) as oxygenation_score
 
-, min(ab_score.ph) as aps3_acidbase_ph
-, min(ab_score.paco2) as aps3_acidbase_paco2
-, min(ab_score.acidbase_score) as acidbase_score
+, (ab_score.ph) as aps3_acidbase_ph
+, (ab_score.paco2) as aps3_acidbase_paco2
+, (ab_score.acidbase_score) as acidbase_score
 
 -- oxygenation parameters
-, min(bg.po2) as pao2_min
-, max(bg.po2) as pao2_max
-, min(bg.pco2) as paco2_min
-, max(bg.pco2) as paco2_max
-, min(bg.pao2fio2) as pao2fio2_min
-, max(bg.pao2fio2) as pao2fio2_max
-, min(coalesce(bg.aado2, bg.aado2_calc)) as aado2_min
-, max(coalesce(bg.aado2, bg.aado2_calc)) as aado2_max
+, bg_agg.pao2_min
+, bg_agg.pao2_max
+, bg_agg.paco2_min
+, bg_agg.paco2_max
+, bg_agg.pao2fio2_min
+, bg_agg.pao2fio2_max
+, bg_agg.aado2_min
+, bg_agg.aado2_max
 
 -- acid-base parameters
-, min(bg.PH) as PH_min
-, max(bg.PH) as PH_max
+, bg_agg.PH_min
+, bg_agg.PH_max
 
-, min(bg.BASEEXCESS) as BASEEXCESS_min
-, max(bg.BASEEXCESS) as BASEEXCESS_max
-, min(bg.BICARBONATE) as BICARBONATE_min
-, max(bg.BICARBONATE) as BICARBONATE_max
-, min(bg.TOTALCO2) as TOTALCO2_min
-, max(bg.TOTALCO2) as TOTALCO2_max
+, bg_agg.BASEEXCESS_min
+, bg_agg.BASEEXCESS_max
+, bg_agg.BICARBONATE_min
+, bg_agg.BICARBONATE_max
+, bg_agg.TOTALCO2_min
+, bg_agg.TOTALCO2_max
 
 -- blood count parameters
-, min(bg.HEMATOCRIT) as HEMATOCRIT_min
-, max(bg.HEMATOCRIT) as HEMATOCRIT_max
-, min(bg.HEMOGLOBIN) as HEMOGLOBIN_min
-, max(bg.HEMOGLOBIN) as HEMOGLOBIN_max
-, min(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_min
-, max(bg.CARBOXYHEMOGLOBIN) as CARBOXYHEMOGLOBIN_max
-, min(bg.METHEMOGLOBIN) as METHEMOGLOBIN_min
-, max(bg.METHEMOGLOBIN) as METHEMOGLOBIN_max
+, bg_agg.HEMATOCRIT_min
+, bg_agg.HEMATOCRIT_max
+, bg_agg.HEMOGLOBIN_min
+, bg_agg.HEMOGLOBIN_max
+, bg_agg.CARBOXYHEMOGLOBIN_min
+, bg_agg.CARBOXYHEMOGLOBIN_max
+, bg_agg.METHEMOGLOBIN_min
+, bg_agg.METHEMOGLOBIN_max
 
 -- chemistry
-, min(bg.CHLORIDE) as CHLORIDE_min
-, max(bg.CHLORIDE) as CHLORIDE_max
-, min(bg.CALCIUM) as CALCIUM_min
-, max(bg.CALCIUM) as CALCIUM_max
-, min(bg.TEMPERATURE) as TEMPERATURE_min
-, max(bg.TEMPERATURE) as TEMPERATURE_max
-, min(bg.POTASSIUM) as POTASSIUM_min
-, max(bg.POTASSIUM) as POTASSIUM_max
-, min(bg.SODIUM) as SODIUM_min
-, max(bg.SODIUM) as SODIUM_max
-, min(bg.LACTATE) as LACTATE_min
-, max(bg.LACTATE) as LACTATE_max
-, min(bg.GLUCOSE) as GLUCOSE_min
-, max(bg.GLUCOSE) as GLUCOSE_max
+, bg_agg.CHLORIDE_min
+, bg_agg.CHLORIDE_max
+, bg_agg.CALCIUM_min
+, bg_agg.CALCIUM_max
+, bg_agg.TEMPERATURE_min
+, bg_agg.TEMPERATURE_max
+, bg_agg.POTASSIUM_min
+, bg_agg.POTASSIUM_max
+, bg_agg.SODIUM_min
+, bg_agg.SODIUM_max
+, bg_agg.LACTATE_min
+, bg_agg.LACTATE_max
+, bg_agg.GLUCOSE_min
+, bg_agg.GLUCOSE_max
 
 -- ventilation stuff that's sometimes input
 -- , INTUBATED
@@ -191,16 +275,11 @@ select ie.subject_id, ie.hadm_id, ie.icustay_id
 -- , O2Flow
 -- , REQUIREDO2
 
-from icustays ie
-left join bg
-  on ie.icustay_id = bg.icustay_id
-  -- restrict it to *only* arterial samples
-  and bg.specimen_pred = 'ART'
+from bg_agg
 left join ab_score
-  on ie.icustay_id = ab_score.icustay_id
+  on bg_agg.icustay_id = ab_score.icustay_id
   and ab_score.rn = 1
 left join oxy_score
-  on ie.icustay_id = oxy_score.icustay_id
+  on bg_agg.icustay_id = oxy_score.icustay_id
   and oxy_score.rn = 1
-group by ie.subject_id, ie.hadm_id, ie.icustay_id
-order by ie.icustay_id;
+order by bg_agg.icustay_id;
