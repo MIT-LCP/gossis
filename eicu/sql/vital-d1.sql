49077a62450167c0df8d0dd89152d53084f03a68
Alistair Johnson <alistairewj@gmail.com>
2017-05-24 12:23:35 -0400

    add limits on vital signs

--- eicu/sql/vital-d1.sql
+++ eicu/sql/vital-d1.sql
@@ -28,20 +28,22 @@ left join
 (
   select
     patientunitstayid
-    , min(heartrate) as heartrate_min
-    , max(heartrate) as heartrate_max
-    , min(respiration) as resprate_min
-    , max(respiration) as resprate_max
-    , min(sao2) as spo2_min
-    , max(sao2) as spo2_max
-    , min(temperature) as temp_min
-    , max(temperature) as temp_max
-    , min(systemicsystolic) as sysbp_invasive_min
-    , max(systemicsystolic) as sysbp_invasive_max
-    , min(systemicdiastolic) as diasbp_invasive_min
-    , max(systemicdiastolic) as diasbp_invasive_max
-    , min(systemicmean) as mbp_invasive_min
-    , max(systemicmean) as mbp_invasive_max
+    , min(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_min
+    , max(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_max
+    , min(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_min
+    , max(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_max
+    , min(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_min
+    , max(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_max
+    , min(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
+               when temperature > 10 and temperature < 50 then temperature else null end) as temp_min
+    , max(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
+               when temperature > 10 and temperature < 50 then temperature else null end) as temp_max
+    , min(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_min
+    , max(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_max
+    , min(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_min
+    , max(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_max
+    , min(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_min
+    , max(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_max
   from vitalperiodic
   -- during the first day of their ICU stay
   where observationoffset >= (-60*1) and observationoffset <= (60*24)
--- eicu/sql/vital-h1.sql
+++ eicu/sql/vital-h1.sql
@@ -26,22 +26,46 @@ select
 from patient pat
 left join
 (
+
+    when itemid in (211,220045) and valuenum > 0 and valuenum < 300 then 1 -- HeartRate
+    -- below case statements just shown for illustration of getting any BP meas
+    -- including them would supercede the non-invasive/invasive case statements
+    -- when itemid in (51,442,455,6701,220179,220050) and valuenum > 0 and valuenum < 400 then 2 -- SysBP
+    -- when itemid in (8368,8440,8441,8555,220180,220051) and valuenum > 0 and valuenum < 300 then 3 -- DiasBP
+    -- when itemid in (456,52,6702,443,220052,220181,225312) and valuenum > 0 and valuenum < 300 then 4 -- MeanBP
+    when itemid in (615,618,220210,224690) and valuenum > 0 and valuenum < 70 then 5 -- RespRate
+    when itemid in (223761,678) and valuenum > 70 and valuenum < 120  then 6 -- TempF, converted to degC in valuenum call
+    when itemid in (223762,676) and valuenum > 10 and valuenum < 50  then 6 -- TempC
+    when itemid in (646,220277) and valuenum > 0 and valuenum <= 100 then 7 -- SpO2
+    when itemid in (807,811,1529,3745,3744,225664,220621,226537) and valuenum > 0 then 8 -- Glucose
+    when itemid in (51,6701,220050) and valuenum > 0 and valuenum < 400 then 9 -- SysBPInv
+    when itemid in (8368,8555,220051) and valuenum > 0 and valuenum < 300 then 10 -- DiasBPInv
+    when itemid in (442,455,220179) and valuenum > 0 and valuenum < 400 then 11 -- SysBPNI
+    when itemid in (8440,8441,220180) and valuenum > 0 and valuenum < 300 then 12 -- DiasBPNI
+    when itemid in (52,6702,220052,225312) and valuenum > 0 and valuenum < 400 then 13 -- MBPInv
+    when itemid in (456,443,220181) and valuenum > 0 and valuenum < 400 then 14 -- MBPNI
+    when itemid in (492,220059) and valuenum > 0 and valuenum < 80 then 15 -- PAPs
+    when itemid in (8448,220060) and valuenum > 0 and valuenum < 80 then 16 -- PAPd
+    when itemid in (491,220061) and valuenum > 0 and valuenum < 80 then 17 -- PAP mean
+
   select
     patientunitstayid
-    , min(heartrate) as heartrate_min
-    , max(heartrate) as heartrate_max
-    , min(respiration) as resprate_min
-    , max(respiration) as resprate_max
-    , min(sao2) as spo2_min
-    , max(sao2) as spo2_max
-    , min(temperature) as temp_min
-    , max(temperature) as temp_max
-    , min(systemicsystolic) as sysbp_invasive_min
-    , max(systemicsystolic) as sysbp_invasive_max
-    , min(systemicdiastolic) as diasbp_invasive_min
-    , max(systemicdiastolic) as diasbp_invasive_max
-    , min(systemicmean) as mbp_invasive_min
-    , max(systemicmean) as mbp_invasive_max
+    , min(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_min
+    , max(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_max
+    , min(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_min
+    , max(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_max
+    , min(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_min
+    , max(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_max
+    , min(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
+               when temperature > 10 and temperature < 50 then temperature else null end) as temp_min
+    , max(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
+               when temperature > 10 and temperature < 50 then temperature else null end) as temp_max
+    , min(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_min
+    , max(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_max
+    , min(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_min
+    , max(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_max
+    , min(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_min
+    , max(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_max
   from vitalperiodic
   -- during the first day of their ICU stay
   where observationoffset >= (-60*1) and observationoffset <= (60*1)
