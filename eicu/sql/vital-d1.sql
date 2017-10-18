ba7baf31c9590207af7c9cbfe53962318036a004
Alistair Johnson <alistairewj@gmail.com>
2017-10-18 12:11:11 -0400

    change threshold ranges

--- eicu/sql/vital-d1.sql
+++ eicu/sql/vital-d1.sql
@@ -28,22 +28,22 @@ left join
 (
   select
     patientunitstayid
-    , min(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_min
-    , max(case when heartrate > 0 and heartrate < 300 then heartrate else null end) as heartrate_max
-    , min(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_min
-    , max(case when respiration > 0 and respiration < 70 then respiration else null end) as resprate_max
-    , min(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_min
-    , max(case when sao2 > 0 and sao2 <= 100 then sao2 else null end) as spo2_max
-    , min(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
-               when temperature > 10 and temperature < 50 then temperature else null end) as temp_min
-    , max(case when temperature > 70 and temperature < 120 then (temperature-32)/1.8
-               when temperature > 10 and temperature < 50 then temperature else null end) as temp_max
-    , min(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_min
-    , max(case when systemicsystolic > 0 and systemicsystolic < 400 then systemicsystolic else null end) as sysbp_invasive_max
-    , min(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_min
-    , max(case when systemicdiastolic > 0 and systemicdiastolic < 300 then systemicdiastolic else null end) as diasbp_invasive_max
-    , min(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_min
-    , max(case when systemicmean > 0 and systemicmean < 300 then systemicmean else null end) as mbp_invasive_max
+    , min(case when heartrate >= 0 and heartrate <= 400 then heartrate else null end) as heartrate_min
+    , max(case when heartrate >= 0 and heartrate <= 400 then heartrate else null end) as heartrate_max
+    , min(case when respiration >= 0 and respiration <= 300 then respiration else null end) as resprate_min
+    , max(case when respiration >= 0 and respiration <= 300 then respiration else null end) as resprate_max
+    , min(case when sao2 >= 0 and sao2 <= 100 then sao2 else null end) as spo2_min
+    , max(case when sao2 >= 0 and sao2 <= 100 then sao2 else null end) as spo2_max
+    , min(case when temperature > (25*1.8+32) and temperature < (46*1.8+32) then (temperature-32)/1.8
+               when temperature > 25 and temperature < 46 then temperature else null end) as temp_min
+    , max(case when temperature > (25*1.8+32) and temperature < (46*1.8+32) then (temperature-32)/1.8
+               when temperature > 25 and temperature < 46 then temperature else null end) as temp_max
+    , min(case when systemicsystolic >= 1 and systemicsystolic <= 300 then systemicsystolic else null end) as sysbp_invasive_min
+    , max(case when systemicsystolic >= 1 and systemicsystolic <= 300 then systemicsystolic else null end) as sysbp_invasive_max
+    , min(case when systemicdiastolic >= 1 and systemicdiastolic <= 200 then systemicdiastolic else null end) as diasbp_invasive_min
+    , max(case when systemicdiastolic >= 1 and systemicdiastolic <= 200 then systemicdiastolic else null end) as diasbp_invasive_max
+    , min(case when systemicmean >= 1 and systemicmean <= 334 then systemicmean else null end) as mbp_invasive_min
+    , max(case when systemicmean >= 1 and systemicmean <= 334 then systemicmean else null end) as mbp_invasive_max
   from vitalperiodic
   -- during the first day of their ICU stay
   where observationoffset >= (-60*1) and observationoffset <= (60*24)
@@ -54,12 +54,12 @@ left join
 (
   select
     patientunitstayid
-    , min(case when noninvasivemean > 0 and noninvasivemean < 300 then noninvasivemean else null end) as mbp_noninvasive_min
-    , max(case when noninvasivemean > 0 and noninvasivemean < 300 then noninvasivemean else null end) as mbp_noninvasive_max
-    , min(case when noninvasivesystolic > 0 and noninvasivesystolic < 400 then noninvasivesystolic else null end) as sysbp_noninvasive_min
-    , max(case when noninvasivesystolic > 0 and noninvasivesystolic < 400 then noninvasivesystolic else null end) as sysbp_noninvasive_max
-    , min(case when noninvasivediastolic > 0 and noninvasivediastolic < 300 then noninvasivediastolic else null end) as diasbp_noninvasive_min
-    , max(case when noninvasivediastolic > 0 and noninvasivediastolic < 300 then noninvasivediastolic else null end) as diasbp_noninvasive_max
+    , min(case when noninvasivemean >= 1 and noninvasivemean <= 334 then noninvasivemean else null end) as mbp_noninvasive_min
+    , max(case when noninvasivemean >= 1 and noninvasivemean <= 334 then noninvasivemean else null end) as mbp_noninvasive_max
+    , min(case when noninvasivesystolic >= 1 and noninvasivesystolic <= 300 then noninvasivesystolic else null end) as sysbp_noninvasive_min
+    , max(case when noninvasivesystolic >= 1 and noninvasivesystolic <= 300 then noninvasivesystolic else null end) as sysbp_noninvasive_max
+    , min(case when noninvasivediastolic >= 1 and noninvasivediastolic <= 200 then noninvasivediastolic else null end) as diasbp_noninvasive_min
+    , max(case when noninvasivediastolic >= 1 and noninvasivediastolic <= 200 then noninvasivediastolic else null end) as diasbp_noninvasive_max
   from vitalaperiodic
   -- during the first day of their ICU stay
   where observationoffset >= (-60*1) and observationoffset <= (60*24)
--- eicu/sql/vital-nc-d1.sql
+++ eicu/sql/vital-nc-d1.sql
@@ -137,31 +137,31 @@ select
   select
     patientunitstayid
   -- lowest
-  , min(case when heartrate > 0 and heartrate < 400 then heartrate else null end) as heartrate_min
-  , min(case when RespiratoryRate > 0 and RespiratoryRate < 80 then RespiratoryRate else null end) as RespiratoryRate_min
-  , min(case when o2saturation > 0 and o2saturation <= 100 then o2saturation else null end) as o2saturation_min
-  , min(case when nibp_systolic > 0 and nibp_systolic < 400 then nibp_systolic else null end) as nibp_systolic_min
-  , min(case when nibp_diastolic > 0 and nibp_diastolic < 400 then nibp_diastolic else null end) as nibp_diastolic_min
-  , min(case when nibp_mean > 0 and nibp_mean < 400 then nibp_mean else null end) as nibp_mean_min
-  , min(case when temperature > 20 and temperature < 50 then temperature else null end) as temperature_min
-  , min(case when ibp_systolic > 0 and ibp_systolic < 400 then ibp_systolic else null end) as ibp_systolic_min
-  , min(case when ibp_diastolic > 0 and ibp_diastolic < 400 then ibp_diastolic else null end) as ibp_diastolic_min
-  , min(case when ibp_mean > 0 and ibp_mean < 400 then ibp_mean else null end) as ibp_mean_min
+  , min(case when heartrate >= 0 and heartrate <= 400 then heartrate else null end) as heartrate_min
+  , min(case when RespiratoryRate >= 0 and RespiratoryRate <= 300 then RespiratoryRate else null end) as RespiratoryRate_min
+  , min(case when o2saturation >= 0 and o2saturation <= 100 then o2saturation else null end) as o2saturation_min
+  , min(case when nibp_systolic >= 1 and nibp_systolic <= 300 then nibp_systolic else null end) as nibp_systolic_min
+  , min(case when nibp_diastolic >= 1 and nibp_diastolic <= 200 then nibp_diastolic else null end) as nibp_diastolic_min
+  , min(case when nibp_mean >= 1 and nibp_mean <= 334 then nibp_mean else null end) as nibp_mean_min
+  , min(case when temperature >= 25 and temperature <= 46 then temperature else null end) as temperature_min
+  , min(case when ibp_systolic >= 1 and ibp_systolic <= 300 then ibp_systolic else null end) as ibp_systolic_min
+  , min(case when ibp_diastolic >= 1 and ibp_diastolic <= 200 then ibp_diastolic else null end) as ibp_diastolic_min
+  , min(case when ibp_mean >= 1 and ibp_mean <= 334 then ibp_mean else null end) as ibp_mean_min
   , min(case when gcs > 2 and gcs < 16 then gcs else null end) as gcs_min
-  , min(case when map > 0 and map < 400 then map else null end) as map_min
+  , min(case when map >= 1 and map <= 334 then map else null end) as map_min
   -- highest
-  , max(case when heartrate > 0 and heartrate < 400 then heartrate else null end) as heartrate_max
-  , max(case when RespiratoryRate > 0 and RespiratoryRate < 80 then RespiratoryRate else null end) as RespiratoryRate_max
-  , max(case when o2saturation > 0 and o2saturation <= 100 then o2saturation else null end) as o2saturation_max
-  , max(case when nibp_systolic > 0 and nibp_systolic < 400 then nibp_systolic else null end) as nibp_systolic_max
-  , max(case when nibp_diastolic > 0 and nibp_diastolic < 400 then nibp_diastolic else null end) as nibp_diastolic_max
-  , max(case when nibp_mean > 0 and nibp_mean < 400 then nibp_mean else null end) as nibp_mean_max
-  , max(case when temperature > 20 and temperature < 50 then temperature else null end) as temperature_max
-  , max(case when ibp_systolic > 0 and ibp_systolic < 400 then ibp_systolic else null end) as ibp_systolic_max
-  , max(case when ibp_diastolic > 0 and ibp_diastolic < 400 then ibp_diastolic else null end) as ibp_diastolic_max
-  , max(case when ibp_mean > 0 and ibp_mean < 400 then ibp_mean else null end) as ibp_mean_max
+  , max(case when heartrate >= 0 and heartrate <= 400 then heartrate else null end) as heartrate_max
+  , max(case when RespiratoryRate >= 0 and RespiratoryRate <= 300 then RespiratoryRate else null end) as RespiratoryRate_max
+  , max(case when o2saturation >= 0 and o2saturation <= 100 then o2saturation else null end) as o2saturation_max
+  , max(case when nibp_systolic >= 1 and nibp_systolic <= 300 then nibp_systolic else null end) as nibp_systolic_max
+  , max(case when nibp_diastolic >= 1 and nibp_diastolic <= 200 then nibp_diastolic else null end) as nibp_diastolic_max
+  , max(case when nibp_mean >= 1 and nibp_mean <= 334 then nibp_mean else null end) as nibp_mean_max
+  , max(case when temperature >= 25 and temperature <= 45 then temperature else null end) as temperature_max
+  , max(case when ibp_systolic >= 1 and ibp_systolic <= 300 then ibp_systolic else null end) as ibp_systolic_max
+  , max(case when ibp_diastolic >= 1 and ibp_diastolic <= 200 then ibp_diastolic else null end) as ibp_diastolic_max
+  , max(case when ibp_mean >= 1 and ibp_mean < 334 then ibp_mean else null end) as ibp_mean_max
   , max(case when gcs > 2 and gcs < 16 then gcs else null end) as gcs_max
-  , max(case when map > 0 and map < 400 then map else null end) as map_max
+  , max(case when map >= 1 and map <= 334 then map else null end) as map_max
   from nc
   group by patientunitstayid
 )
